edcba0ffeaeac20dfacb28ead1110dfa9adaa5de
Uryel Farias <uryel.farias@gmail.com>
2025-02-03 11:01:21 -0300

    Iniciando o projeto sorteador de numeros

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear() {
+    let quantidade = document.querySelector('#quantidade').value;
+    let aPartirDe = document.querySelector('#de').value;
+    let ate = document.querySelector('#ate').value;
+
+    console.log(quantidade);
+    console.log(aPartirDe);
+    console.log(ate);
+
+}
