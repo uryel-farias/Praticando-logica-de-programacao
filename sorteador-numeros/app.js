e098b3b73dac2dc4cba93bb07daec74f22ac2920
Uryel Farias <uryel.farias@gmail.com>
2025-02-04 11:01:51 -0300

    criando a função de sortear números aleatórios e mostrar na tela os numeros.

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,10 +1,20 @@
 function sortear() {
-    let quantidade = document.querySelector('#quantidade').value;
-    let aPartirDe = document.querySelector('#de').value;
-    let ate = document.querySelector('#ate').value;
+    let quantidade = parseInt(document.querySelector('#quantidade').value);
+    let aPartirDe = parseInt(document.querySelector('#de').value);
+    let ate = parseInt(document.querySelector('#ate').value);
 
-    console.log(quantidade);
-    console.log(aPartirDe);
-    console.log(ate);
+    let numerosSortiados = [];
+    let numero;
 
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(aPartirDe, ate);
+        numerosSortiados.push(numero);
+    }
+
+    alert(numerosSortiados);
+
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
