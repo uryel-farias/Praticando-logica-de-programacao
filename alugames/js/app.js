69e72a8c82c4245293c38ee7eb85c307a01bbc77
Uryel Farias <uryel.farias@gmail.com>
2025-02-05 14:52:02 -0300

    Selecionando as variaves do projeto alugames.

---
+++ alugames/js/app.js
@@ -0,0 +1,7 @@
+function alterarStatus(status_id) {
+    let gameCliacado = document.querySelector(`#game-${status_id}`);
+    let imagem = gameCliacado.querySelector('.dashboard__item__img');
+    let botao = gameCliacado.querySelector('.dashboard__item__button');
+
+    
+}
