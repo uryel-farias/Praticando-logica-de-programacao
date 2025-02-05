a8e6b24d774c0d9c449fb8503e5c03cf6a6dfeac
Uryel Farias <uryel.farias@gmail.com>
2025-02-05 15:05:34 -0300

    alterando as clases do alugames.

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -3,5 +3,17 @@ function alterarStatus(status_id) {
     let imagem = gameCliacado.querySelector('.dashboard__item__img');
     let botao = gameCliacado.querySelector('.dashboard__item__button');
 
-    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+
+
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+
+
+    }
 }
