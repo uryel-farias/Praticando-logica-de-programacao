c90f94b1281c5b3d71be509a7bbc55d6ae3a59d4
Uryel Farias <uryel.farias@gmail.com>
2025-02-06 10:27:03 -0300

    iniciando o projeto carrinho -compras e recuperando os valores na função adicionar.

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,16 @@
+function adicionar() {
+//recuperar valores - nome, quant, valores
+let nomeProduto = document.querySelector('#produto');
+let quantidade = document.querySelector('#quantidade');
+
+//alert(nomeProduto.value);
+//calcular preço = subtotal initário
+//atualizar valor total
+
+}
+
+function limpar() {
+//limpar carrinho
+//limpar valores
+
+}
