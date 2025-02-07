bf79a011b370a2efb0590f1bae0c7f2c286f22d2
Uryel Farias <uryel.farias@gmail.com>
2025-02-07 11:34:30 -0300

    Atualizando o valor total do carrinho e implementando o botal de limpar.

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,5 @@
+let precoTotal= 0;
+
 function adicionar() {
 //recuperar valores - nome, quant, valores
     let produto = document.querySelector('#produto').value;
@@ -15,10 +17,33 @@ function adicionar() {
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>
     `;
+
+    quantidade.value = '';
+
+    precoTotal += preco;
+    let carrinhoTotal = document.querySelector('#valor-total');
+    carrinhoTotal.innerHTML = `
+    <span class="texto-azul" id="valor-total">R$${precoTotal}</span>
+    `;
+    
+    
 }
 
 function limpar() {
 //limpar carrinho
+    let listaProdutos = document.querySelector('#lista-produtos');
+    listaProdutos.innerHTML = `
+          <section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          
+        </section>
+      </section>
+    `
 //limpar valores
+    let carrinhoTotal = document.querySelector('#valor-total');
+    carrinhoTotal.innerHTML = `
+    <span class="texto-azul" id="valor-total">R$0,00</span>
+    `;
 
+    quantidade.value = '';
 }
