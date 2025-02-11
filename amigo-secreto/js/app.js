ceae147791140feb5baf60cd4deab99210871bb8
Uryel Farias <uryel.farias@gmail.com>
2025-02-11 09:50:51 -0300

    implementandoa logica do sorteio.

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -17,7 +17,16 @@ function adicionar() {
 
 function sortear() {
     embaralha(amigos);
-    
+    let sorteio = document.querySelector('#lista-sorteio');
+
+    for(let i = 0; i < amigos.length; i++ ) {
+        if(i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>'
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[ i+1 ] + '<br>'
+        }
+        
+    }
 
 }
 
