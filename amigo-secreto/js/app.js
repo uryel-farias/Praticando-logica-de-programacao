84a2c562d949e0bf21118cab8d401a8d3e792a52
Uryel Farias <uryel.farias@gmail.com>
2025-02-11 09:18:07 -0300

    adicionando a função embaralhar na lista de amigos.

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,6 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.querySelector('#nome-amigo');
     let lista = document.querySelector('#lista-amigos');
+    
+    amigos.push(amigo.value);
 
     if(lista.textContent == ''){
         lista.textContent = amigo.value;
@@ -12,9 +16,23 @@ function adicionar() {
 }
 
 function sortear() {
+    embaralha(amigos);
+    
 
 }
 
 function reiniciar() {
 
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
